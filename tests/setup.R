a5b021713e270d6d58bb4f8519da0976ead685ae
Anthony Damico <ajdamico@gmail.com>
2017-09-19 23:23:14 -0400

    sample ces

--- tests/setup.R
+++ tests/setup.R
@@ -1,4 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-lodown( "ces" , output_dir = file.path( getwd() ) )
+
+ces_cat <-
+	get_catalog( "ces" ,
+		output_dir = file.path( getwd() ) )
+
+# sample 75% of the records
+which_records <- sample( seq( nrow( ces_cat ) ) , round( nrow( ces_cat ) * 0.75 ) )
+
+# always sample year == 2016
+ces_cat <- unique( rbind( ces_cat[ which_records , ] , subset( ces_cat , year == 2016 ) ) )
+
+lodown( "ces" , ces_cat )
